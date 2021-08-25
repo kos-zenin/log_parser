047d55a50f80142d37f0c3361c00524200843a71
kos-zenin <kos.zenin@gmail.com>
2021-08-25 17:13:52 +0100

    Add file reader implementation

--- lib/files/readers/log_reader.rb
+++ lib/files/readers/log_reader.rb
@@ -3,11 +3,32 @@
 module Files
   module Readers
     class LogReader
-      def initialize(file)
+      WHITELIST_EXTENSIONS = %w[.log].freeze
+
+      def initialize(file, file_proxy: File)
         @file = file
+        @file_proxy = file_proxy
       end
 
-      def call; end
+      def call
+        validate!(@file)
+
+        open_file(@file).lazy.each do |line|
+          yield line.split(' ')
+        end
+      end
+
+      private
+
+      def open_file(file)
+        @file_proxy.open(file, 'rb')
+      rescue Errno::EACCES, Errno::ENOENT
+        raise ArgumentError, 'Check file permissions'
+      end
+
+      def validate!(file)
+        raise ArgumentError, 'Check file extension' unless WHITELIST_EXTENSIONS.include?(@file_proxy.extname(file))
+      end
     end
   end
 end
