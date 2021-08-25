7a4e543cf0e8962bf8102766601364fcfe790716
kos-zenin <kos.zenin@gmail.com>
2021-08-25 17:25:52 +0100

    Add datum Visit implementation

--- lib/datum/visit.rb
+++ lib/datum/visit.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 module Datum
-  class Visit
+  class Visit < Struct.new(:route, :ip)
+    ROUTE_REGEX = %r{^/\w+(/\w+)*$}.freeze
+    IP_REGEX = /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/.freeze
+
+    def valid?
+      route =~ ROUTE_REGEX && ip =~ IP_REGEX
+    end
   end
 end
