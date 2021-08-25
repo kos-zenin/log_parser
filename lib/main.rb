c4b165a5efabae2a09ad4418fe1c518f98cc68cd
kos-zenin <kos.zenin@gmail.com>
2021-08-25 17:02:03 +0100

    Add main entry point implementation

--- lib/main.rb
+++ lib/main.rb
@@ -1,12 +1,30 @@
 # frozen_string_literal: true
 
 class Main
-  def initialize(file, logs_reader: nil, analyzers: [], reporter: nil)
-    @file = file
-    @logs_reader = logs_reader
+  ANALYZERS_MAPPING = {
+    count: ::Analyzers::CountAnalyzer,
+    uniq: ::Analyzers::UniqAnalyzer
+  }.freeze
+
+  def initialize(file, logs_reader: nil, analyzers: [], reporter: ::Reporters::Stdout.new)
+    @logs_reader = logs_reader.new(file)
     @analyzers = analyzers
     @reporter = reporter
   end
 
-  def call; end
+  def call
+    collect_visits.then do |visits|
+      @analyzers.each do |analyzer|
+        ANALYZERS_MAPPING.fetch(analyzer).new(visits).call.then { @reporter.call(_1) }
+      end
+    end
+  end
+
+  private
+
+  def collect_visits
+    @logs_reader.call do |route, ip|
+      visits << ::Datum::Visit.new(route, ip)
+    end
+  end
 end
