338ba99dac3a661aa10054f3461613a1b85e4db0
kos-zenin <kos.zenin@gmail.com>
2021-08-25 16:22:44 +0100

    Add integration specs for main entry point script

---
+++ spec/bin/parser_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+describe 'parse' do
+  context 'integration tests' do
+    context 'when no argument were provided' do
+      it 'writes to stderr an error' do
+        expect { system('bin/parser.rb') }.to output("correct usage is 'bin/bundle webserver.log'\n").to_stderr_from_any_process
+      end
+    end
+
+    context 'when correct arguments' do
+      let(:expected_output) do
+        %{
+          /about/2 90 views
+          /contact 89 views
+          /index 82 views
+          /about 81 views
+          /help_page/1 80 views
+          /home 78 views
+
+          /contact 23 views
+          /index 23 views
+          /help_page/1 23 unique views
+          /home 23 views
+          /about/2 22 views
+          /about 21 views
+        }
+      end
+
+      it 'runs the script' do
+        expect { system('bin/parser.rb files/webserver.log') }.to output(expected_output).to_stdout_from_any_process
+      end
+    end
+  end
+end
